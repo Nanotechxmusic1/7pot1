14a80cff927c3b72172b7ce7657fad5e77e01c4e
Nanotechxmusic1 <nanox.techx.music@gmail.com>
2025-04-28 02:56:16 +0530

    Add views/partials/nav_user.php 1745789173819

---
+++ views/partials/nav_user.php
@@ -0,0 +1,11 @@
+<nav class="bg-black border-b border-yellow-400 p-4 flex justify-between items-center">
+    <a href="dashboard.php" class="text-2xl font-bold text-yellow-400 hover:text-yellow-300">7Pot Games</a>
+    <ul class="flex space-x-6 text-yellow-400">
+        <li><a href="dashboard.php" class="hover:text-yellow-300">Dashboard</a></li>
+        <li><a href="results.php" class="hover:text-yellow-300">Results</a></li>
+        <li><a href="bet.php?game_id=1" class="hover:text-yellow-300">Place Bet</a></li>
+        <li><a href="withdrawal.php" class="hover:text-yellow-300">Withdraw</a></li>
+        <li><a href="recharge.php" class="hover:text-yellow-300">Recharge</a></li>
+        <li><a href="logout.php" class="hover:text-yellow-300">Logout</a></li>
+    </ul>
+</nav>
