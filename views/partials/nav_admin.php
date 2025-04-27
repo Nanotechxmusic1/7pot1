034a6cf33fd3aba2e24183a02b1d98e64fb6bbd7
Nanotechxmusic1 <nanox.techx.music@gmail.com>
2025-04-28 02:56:15 +0530

    Add views/partials/nav_admin.php 1745789172584

---
+++ views/partials/nav_admin.php
@@ -0,0 +1,13 @@
+<nav class="bg-black border-b border-yellow-400 p-4 flex justify-between items-center">
+    <a href="admin_dashboard.php" class="text-2xl font-bold text-yellow-400 hover:text-yellow-300">7Pot Games Admin</a>
+    <ul class="flex space-x-6 text-yellow-400">
+        <li><a href="admin_dashboard.php" class="hover:text-yellow-300">Dashboard</a></li>
+        <li><a href="games.php" class="hover:text-yellow-300">Games</a></li>
+        <li><a href="declare_result.php" class="hover:text-yellow-300">Declare Result</a></li>
+        <li><a href="users.php" class="hover:text-yellow-300">Users</a></li>
+        <li><a href="withdrawals.php" class="hover:text-yellow-300">Withdrawals</a></li>
+        <li><a href="recharges.php" class="hover:text-yellow-300">Recharges</a></li>
+        <li><a href="reports.php" class="hover:text-yellow-300">Reports</a></li>
+        <li><a href="admin_logout.php" class="hover:text-yellow-300">Logout</a></li>
+    </ul>
+</nav>
